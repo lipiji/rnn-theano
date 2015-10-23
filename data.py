5f01e28c6994a4841f85f1800fdf66768fbd0829
Piji <pagelee.sd@gmail.com>
2015-10-23 15:42:45 +0800

    clean codes

--- data.py
+++ data.py
@@ -3,7 +3,6 @@ import sys
 import os
 import numpy as np
 
-datasets_dir = '/misc/projdata12/info_fil/pjli/data/'
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 
 def char_sequence():
@@ -30,5 +29,5 @@ def char_sequence():
         for j in range(0, len(line)):
             x[j, w2i[line[j]]] = 1
         seqs.append(x)
-    print len(w2i)
+    print "#dic = " + str(len(w2i))
     return seqs, i2w, w2i
