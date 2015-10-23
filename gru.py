4012b611aa16174b7940ba5f92464381850ff432
Piji <pagelee.sd@gmail.com>
2015-10-23 19:56:40 +0800

    move the nn logic into cell class

--- gru.py
+++ gru.py
@@ -36,8 +36,7 @@ class Cell(object):
         self.W_hy = init_weights((self.in_size, self.out_size))
         self.b_y = init_bias(self.in_size)
        
-        ###
-
+        # for gradients
         self.gW_xr = init_gradws((self.out_size, self.in_size))
         self.gW_hr = init_gradws((self.out_size, self.out_size))
         self.gb_r = init_bias(self.out_size)
@@ -52,31 +51,101 @@ class Cell(object):
 
         self.gW_hy = init_gradws((self.in_size, self.out_size))
         self.gb_y = init_bias(self.in_size)
+    
+        def _active(x, pre_h):
+            r = T.nnet.sigmoid(T.dot(self.W_xr, x) + T.dot(self.W_hr, pre_h) + self.b_r)
+            z = T.nnet.sigmoid(T.dot(self.W_xz, x) + T.dot(self.W_hz, pre_h) + self.b_z)
+            gh = T.tanh(T.dot(self.W_xh, x) + T.dot(self.W_hh, r * pre_h) + self.b_h)
+            h = z * pre_h + (1 - z) * gh
+            return r, z, gh, h 
+        x = T.matrix("x")
+        pre_h = T.matrix("pre_h")
+        self.active = theano.function(
+            inputs = [x, pre_h],
+            outputs = _active(x, pre_h)
+        )
+        
+        def _predict(h):
+            y = T.nnet.softmax((T.dot(self.W_hy, h) + self.b_y).T).T
+            return y
+        h = T.matrix("h")
+        self.predict = theano.function(
+            inputs = [h],
+            outputs = _predict(h)
+        )
+        
+        def _derive(y, py, r, z, gh, pre_h, post_dh, post_dgh, post_dr, post_dz, post_r):
+            dy = py - y;
+            dh = T.dot(self.W_hy.T, dy) \
+                + T.dot(self.W_hr.T, post_dr) \
+                + T.dot(self.W_hz.T, post_dz) \
+                + T.dot(self.W_hh.T, post_dgh * post_r) \
+                + post_dh * z
+            dgh = dh * (1 - z) * (1 - gh ** 2)
+            dr = T.dot(self.W_hh.T, dgh * pre_h) * ((1 - r) * r)
+            dz = (1 - dh * (gh - pre_h)) * ((1 - z) * z)
+            return dy, dh, dgh, dr, dz
+        y, py, r, z, gh, post_dh, post_dgh, post_dr, post_dz, post_r = \
+            T.matrices("y", "py", "r", "z", "gh", "post_dh", "post_dgh", "post_dr", "post_dz", "post_r")
+        self.derive = theano.function(
+            inputs = [y, py, r, z, gh, pre_h, post_dh, post_dgh, post_dr, post_dz, post_r],
+            outputs = _derive(y, py, r, z, gh, pre_h, post_dh, post_dgh, post_dr, post_dz, post_r)
+        )
 
-           
-    def active(self, x, pre_h):
-        r = T.nnet.sigmoid(T.dot(self.W_xr, x) + T.dot(self.W_hr, pre_h) + self.b_r)
-        z = T.nnet.sigmoid(T.dot(self.W_xz, x) + T.dot(self.W_hz, pre_h) + self.b_z)
-        gh = T.tanh(T.dot(self.W_xh, x) + T.dot(self.W_hh, r * pre_h) + self.b_h)
-        h = z * pre_h + (1 - z) * gh
-        return r, z, gh, h 
+        dy, dz, dr, dgh = T.matrices("dy", "dz", "dr", "dgh")
+        updates_grad = [(self.gW_xr, self.gW_xr + T.dot(dr, x.T)),
+               (self.gW_xz, self.gW_xz + T.dot(dz, x.T)),
+               (self.gW_xh, self.gW_xh + T.dot(dgh, x.T)),
+               (self.gW_hr, self.gW_hr + T.dot(dr, pre_h.T)),
+               (self.gW_hz, self.gW_hz + T.dot(dz, pre_h.T)),
+               (self.gW_hh, self.gW_hh + T.dot(dgh, (r * pre_h).T)),
+               (self.gW_hy, self.gW_hy + T.dot(dy, h.T)),
+               (self.gb_r, self.gb_r + dr),
+               (self.gb_z, self.gb_z + dz),
+               (self.gb_h, self.gb_h + dgh),
+               (self.gb_y, self.gb_y + dy)]
+        self.grad = theano.function(
+            inputs = [x, r, pre_h, h, dy, dz, dr, dgh],
+            updates = updates_grad
+        )
 
-    def predict(self, h):
-        y = T.nnet.softmax((T.dot(self.W_hy, h) + self.b_y).T).T
-        return y
+        updates_clear = [
+               (self.gW_xr, self.gW_xr * 0),
+               (self.gW_xz, self.gW_xz * 0),
+               (self.gW_xh, self.gW_xh * 0),
+               (self.gW_hr, self.gW_hr * 0),
+               (self.gW_hz, self.gW_hz * 0),
+               (self.gW_hh, self.gW_hh * 0),
+               (self.gW_hy, self.gW_hy * 0),
+               (self.gb_r, self.gb_r * 0),
+               (self.gb_z, self.gb_z * 0),
+               (self.gb_h, self.gb_h * 0),
+               (self.gb_y, self.gb_y * 0)]
+        self.clear_grad = theano.function(
+            inputs = [],
+            updates = updates_clear
+        )
 
-    def derive(self, y, py, r, z, gh, pre_h, post_dh, post_dgh, post_dr, post_dz, post_r):
-        dy = py - y;
-        dh = T.dot(self.W_hy.T, dy) \
-           + T.dot(self.W_hr.T, post_dr) \
-           + T.dot(self.W_hz.T, post_dz) \
-           + T.dot(self.W_hh.T, post_dgh * post_r) \
-           + post_dh * z
-        dgh = dh * (1 - z) * (1 - gh ** 2)
-        dr = T.dot(self.W_hh.T, dgh * pre_h) * ((1 - r) * r)
-        dz = (1 - dh * (gh - pre_h)) * ((1 - z) * z)
-        return dy, dh, dgh, dr, dz
-
+        lr = T.scalar()
+        t = T.scalar()
+        tm1 = T.scalar()
+        updates_w = [
+               (self.W_xr, self.W_xr - self.gW_xr * lr / t),
+               (self.W_xz, self.W_xz - self.gW_xz * lr / t),
+               (self.W_xh, self.W_xh - self.gW_xh * lr / t),
+               (self.W_hr, self.W_hr - self.gW_hr * lr / tm1),
+               (self.W_hz, self.W_hz - self.gW_hz * lr / tm1),
+               (self.W_hh, self.W_hh - self.gW_hh * lr / tm1),
+               (self.W_hy, self.W_hy - self.gW_hy * lr / t),
+               (self.b_r, self.b_r - self.gb_r * lr / t),
+               (self.b_z, self.b_z - self.gb_z * lr / t),
+               (self.b_h, self.b_h - self.gb_h * lr / t),
+               (self.b_y, self.b_y - self.gb_y * lr / t)]
+        self.update = theano.function(
+            inputs = [lr, t, tm1],
+            updates = updates_w
+        )
+        
 def rmse(py, y):
     return np.sqrt(np.mean((py - y) ** 2))
 
@@ -94,79 +163,6 @@ def train():
     
     cell = Cell((len(w2i), h_size))
     
-    x = T.matrix("x")
-    pre_h = T.matrix("pre_h")
-    active = theano.function(
-        inputs = [x, pre_h],
-        outputs = cell.active(x, pre_h)
-    )
-
-    h = T.matrix("h")
-    predict = theano.function(
-        inputs = [h],
-        outputs = cell.predict(h)
-    )
-
-    y, py, r, z, gh, post_dh, post_dgh, post_dr, post_dz, post_r = \
-        T.matrices("y", "py", "r", "z", "gh", "post_dh", "post_dgh", "post_dr", "post_dz", "post_r")
-    derive = theano.function(
-        inputs = [y, py, r, z, gh, pre_h, post_dh, post_dgh, post_dr, post_dz, post_r],
-        outputs = cell.derive(y, py, r, z, gh, pre_h, post_dh, post_dgh, post_dr, post_dz, post_r)
-    )
-
-    dy, dz, dr, dgh = T.matrices("dy", "dz", "dr", "dgh")
-    updates_grad = [(cell.gW_xr, cell.gW_xr + T.dot(dr, x.T)),
-               (cell.gW_xz, cell.gW_xz + T.dot(dz, x.T)),
-               (cell.gW_xh, cell.gW_xh + T.dot(dgh, x.T)),
-               (cell.gW_hr, cell.gW_hr + T.dot(dr, pre_h.T)),
-               (cell.gW_hz, cell.gW_hz + T.dot(dz, pre_h.T)),
-               (cell.gW_hh, cell.gW_hh + T.dot(dgh, (r * pre_h).T)),
-               (cell.gW_hy, cell.gW_hy + T.dot(dy, h.T)),
-               (cell.gb_r, cell.gb_r + dr),
-               (cell.gb_z, cell.gb_z + dz),
-               (cell.gb_h, cell.gb_h + dgh),
-               (cell.gb_y, cell.gb_y + dy)]
-    grad = theano.function(
-        inputs = [x, r, pre_h, h, dy, dz, dr, dgh],
-        updates = updates_grad
-    )
-
-    ## how to clear the content?
-    updates_clear = [(cell.gW_xr, cell.gW_xr * 0),
-               (cell.gW_xz, cell.gW_xz * 0),
-               (cell.gW_xh, cell.gW_xh * 0),
-               (cell.gW_hr, cell.gW_hr * 0),
-               (cell.gW_hz, cell.gW_hz * 0),
-               (cell.gW_hh, cell.gW_hh * 0),
-               (cell.gW_hy, cell.gW_hy * 0),
-               (cell.gb_r, cell.gb_r * 0),
-               (cell.gb_z, cell.gb_z * 0),
-               (cell.gb_h, cell.gb_h * 0),
-               (cell.gb_y, cell.gb_y * 0)]
-    clear_grad = theano.function(
-        inputs = [],
-        updates = updates_clear
-    )
-
-    lr = T.scalar()
-    t = T.scalar()
-    tm1 = T.scalar()
-    updates_w = [(cell.W_xr, cell.W_xr - cell.gW_xr * lr / t),
-               (cell.W_xz, cell.W_xz - cell.gW_xz * lr / t),
-               (cell.W_xh, cell.W_xh - cell.gW_xh * lr / t),
-               (cell.W_hr, cell.W_hr - cell.gW_hr * lr / tm1),
-               (cell.W_hz, cell.W_hz - cell.gW_hz * lr / tm1),
-               (cell.W_hh, cell.W_hh - cell.gW_hh * lr / tm1),
-               (cell.W_hy, cell.W_hy - cell.gW_hy * lr / t),
-               (cell.b_r, cell.b_r - cell.gb_r * lr / t),
-               (cell.b_z, cell.b_z - cell.gb_z * lr / t),
-               (cell.b_h, cell.b_h - cell.gb_h * lr / t),
-               (cell.b_y, cell.b_y - cell.gb_y * lr / t)]
-    update = theano.function(
-        inputs = [lr, t, tm1],
-        updates = updates_w
-    )
-
     start = time.time()
 
     for i in xrange(100):
@@ -180,13 +176,13 @@ def train():
                 x = np.reshape(seqs[s][t], (len(w2i), 1))
                 acts["x" + str(t)] = x
                 pre_h = get_pre_h(t, h_size, acts)
-                r, z, gh, h = active(x, pre_h)
+                r, z, gh, h = cell.active(x, pre_h)
                 acts["r" + str(t)] = r
                 acts["z" + str(t)] = z
                 acts["gh" + str(t)] = gh
                 acts["h" + str(t)] = h
 
-                py = predict(h)
+                py = cell.predict(h)
                 y = np.reshape(seqs[s][t + 1], (len(w2i), 1))
                 acts["py" + str(t)] = py
                 acts["y" + str(t)] = y
@@ -219,7 +215,7 @@ def train():
                     post_dz =acts["dz" + str(t + 1)]
                     post_r = acts["r" + str(t + 1)]
                 
-                dy, dh, dgh, dr, dz = derive(y, py, r, z, gh, pre_h,
+                dy, dh, dgh, dr, dz = cell.derive(y, py, r, z, gh, pre_h,
                                             post_dh, post_dgh, post_dr, post_dz, post_r)
                 acts["dy" + str(t)] = dy
                 acts["dh" + str(t)] = dh
@@ -228,7 +224,7 @@ def train():
                 acts["dz" + str(t)] = dz
             
             ##grad
-            clear_grad()
+            cell.clear_grad()
             for t in xrange(len(seqs[s]) - 1):
                 x = acts["x" + str(t)]
                 pre_h = get_pre_h(t, h_size, acts)
@@ -240,12 +236,12 @@ def train():
                 dz = acts["dz" + str(t)]
                 dgh = acts["dgh" + str(t)]
 
-                grad(x, r, pre_h, h, dy, dz, dr, dgh)
+                cell.grad(x, r, pre_h, h, dy, dz, dr, dgh)
            
             tm1 = t - 1
             if tm1 < 1:
                 tm1 = 1
-            update(learning_rate, t, tm1);
+            cell.update(learning_rate, t, tm1);
         #print "\nIter = " + str(i) + ", error = " + str(error / len(seqs))
     print time.time() - start
    
