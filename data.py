a3775a2ec9654c0f0bfddf3252ea98644d85507c
Piji <pagelee.sd@gmail.com>
2015-10-30 17:18:08 +0800

    fix logistic

--- data.py
+++ data.py
@@ -36,6 +36,7 @@ def char_sequence():
     return seqs, i2w, w2i
 
 
+#data: http://deeplearning.net/data/mnist/mnist.pkl.gz
 def mnist(batch_size = 1):
     def batch(X, Y, batch_size):
         data_xy = {}
@@ -58,9 +59,10 @@ def mnist(batch_size = 1):
     train_set, valid_set, test_set = cPickle.load(f)
     f.close()
     return batch(train_set[0], train_set[1], batch_size), \
-           batch(valid_set[0], valid_set[1], 1), \
-           batch(test_set[0], test_set[1], 1)
+           batch(valid_set[0], valid_set[1], batch_size), \
+           batch(test_set[0], test_set[1], batch_size)
 
+#data: http://deeplearning.net/data/mnist/mnist.pkl.gz
 def shared_mnist():
     def shared_dataset(data_xy):
         data_x, data_y = data_xy
--- main.py
+++ main.py
@@ -11,34 +11,19 @@ import data
 #seqs, i2w, w2i = data.char_sequence()
 
 lr = 0.2
-batch_size = 200
-datasets = data.shared_mnist()
-train_set_x, train_set_y = datasets[0]
-valid_set_x, valid_set_y = datasets[1]
-test_set_x, test_set_y = datasets[2]
+batch_size = 20
+train_set, valid_set, test_set  = data.mnist(batch_size)
 
-# compute number of minibatches for training, validation and testing
-n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / 1
-n_test_batches = test_set_x.get_value(borrow=True).shape[0] / 1
-
-hidden_size = [400]
+hidden_size = [500]
 layers = []
 
 #layers = [GRULayer(len(w2i), hidden_size[0]),
 #          GRULayer(hidden_size[0], hidden_size[1]),
 #          SoftmaxLayer(hidden_size[len(hidden_size) - 1], len(w2i))]
 
-dim_x = train_set_x.get_value(borrow=True).shape[1]
-dim_y = train_set_y.get_value(borrow=True).shape[1]
+dim_x = train_set[0][0][0].shape[1]
+dim_y = train_set[0][1][0].shape[1]
 print dim_x, dim_y
-
-np_train_x = np.asmatrix(train_set_x.get_value(borrow=True))
-np_train_y = np.asmatrix(train_set_y.get_value(borrow=True))
-np_valid_x = np.asmatrix(valid_set_x.get_value(borrow=True))
-np_valid_y = np.asmatrix(valid_set_y.get_value(borrow=True))
-np_test_x = np.asmatrix(test_set_x.get_value(borrow=True))
-np_test_y = np.asmatrix(test_set_y.get_value(borrow=True))
 
 for lay in xrange(len(hidden_size)):
     if lay == 0:
@@ -52,22 +37,25 @@ nn = NN(layers)
 
 #batch train
 start = time.time()
-for i in xrange(5):
+for i in xrange(100):
     acc = 0.0
     in_start = time.time()
-    for index in xrange(n_train_batches):
-        X = np_train_x[index * batch_size: (index + 1) * batch_size, :]
-        Y = np_train_y[index * batch_size: (index + 1) * batch_size, :]
+    for index, data_xy in train_set.items():
+        X = data_xy[0]
+        Y = data_xy[1]
         nn.batch_train(X, Y, lr)
     in_time = time.time() - in_start
 
-    for index in xrange(n_valid_batches):
-        x = np_valid_x[index, :]
-        y = np_valid_y[index, :]
-        label = np.argmax(y)
-        p_label = np.argmax(nn.predict(x))
-        if label == p_label:
-            acc += 1
+    num_x = 0.0
+    for index, data_xy in valid_set.items():
+        X = data_xy[0]
+        Y = data_xy[1]
+        label = np.argmax(Y, axis=1)
+        p_label = np.argmax(nn.predict(X), axis=1)
+        for c in xrange(len(label)):
+            num_x += 1
+            if label[c] == p_label[c]:
+                acc += 1
 
-    print i, acc / n_valid_batches, in_time
+    print i, acc / num_x, in_time
 print time.time() - start
